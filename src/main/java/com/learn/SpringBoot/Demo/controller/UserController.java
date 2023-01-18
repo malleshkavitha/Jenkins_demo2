29ca29a01a390937423cbd263d6f170c9fbc286d
Kavitha <kavitha.m@stackroute.in>
2023-01-18 11:38:40 +0530

    first commit

---
+++ src/main/java/com/learn/SpringBoot/Demo/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.learn.SpringBoot.Demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class UserController {
+
+
+    @GetMapping("users")
+    public String demo(){
+        return "Api running";
+    }
+
+}
